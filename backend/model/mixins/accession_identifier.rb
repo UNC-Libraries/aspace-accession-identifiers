d306938a625d7654e84d184bcbf90aca124a1eda
Christina Cortland <crosalie@live.unc.edu>
2019-11-11 20:36:23 -0500

    Save accession id_0 to identifier in db on create too

--- backend/model/mixins/accession_identifier.rb
+++ backend/model/mixins/accession_identifier.rb
@@ -1,9 +1,24 @@
 module AccessionIdentifier
 
+  def self.included(base)
+    base.extend(ClassMethods)
+  end
+
   def update_from_json(json, *other_args)
+    # Update identifier in db from id_0 in json
     @id_0 = json[:id_0]
 
     super
+  end
+
+  module ClassMethods
+    def create_from_json(json, opts={})
+      # Kinda clunky way of forcing identifier in db to get set from id_0
+      # Otherwise, bc id_0 is not a dynamic_enum in schema, id_0 from json just gets sent to id_0_id
+      opts.merge!(identifier: JSON([json[:id_0], json[:id_1], json[:id_2], json[:id_3]]))
+
+      super
+    end
   end
 
 end
